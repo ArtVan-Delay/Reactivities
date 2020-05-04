0a471abf4e49fbaa05826583008bf755db6f1b29
ArtVan-Delay <festivus4all@yahoo.com>
2020-05-04 11:26:26 -0500

    End of Section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { setTimeout } from "timers";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser, IUserFormValues } from "../models/user";
+import { IProfile, IPhoto } from "../models/profile";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -64,7 +65,15 @@ const requests = {
     axios
       .delete(url)
       .then(sleep(1000))
-      .then(responseBody)
+      .then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post(url, formData, {
+      headers: {'Content-type': 'multipart/form-data'}
+    }).then(responseBody) 
+  }
+  
 };
 
 const Activities = {
@@ -84,7 +93,15 @@ const User = {
   register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register/`, user),
 }
 
+const Profiles = {
+  get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id:string) => requests.del(`/photos/${id}`)
+}
+
 export default {
   Activities,
-  User
+  User,
+  Profiles
 };
